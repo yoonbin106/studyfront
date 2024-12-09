d1ec52bf331caacfaabda4b59f4dc36a5165181f
Biin106 <glfn30832@naver.com>
2024-12-10 03:37:21 +0900

    axios 방식으로 변경

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -7,10 +8,8 @@ const App = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        // 안드로이드 에뮬레이터 -> localhost 대신 10.0.2.2 사용
-        const response = await fetch('http://10.0.2.2:3000/data');  // NestJS 서버 주소
-        const result = await response.json();
-        setData(result);
+        const response = await axios.get('http://10.0.2.2:3000/data');  // NestJS 서버 주소
+        setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
